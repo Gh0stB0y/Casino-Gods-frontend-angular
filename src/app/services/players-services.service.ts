6a082b688e0eb3915932373609c49df3b1bb2357
Gh0stB0y <118272003+Gh0stB0y@users.noreply.github.com>
2023-09-21 13:24:16 +0200

    Revert "	modified:   src/app/components/games/Dragon/dragon/dragon.component.ts"
    This reverts commit 192d8ff00e603fdea118c1dc48c359d89ff0ca8d.

--- src/app/services/players-services.service.ts
+++ src/app/services/players-services.service.ts
@@ -17,24 +17,33 @@ export class PlayersServicesService {
     return this.http.get<Player[]>(this.baseApiUrl+'/api/Players');
   }
   addPlayer(addPlayerRequest:PlayerSignUp):Observable<PlayerSignUp>{
-    return this.http.post<PlayerSignUp>(this.baseApiUrl+'/api/Players/Register',addPlayerRequest);
+    return this.http.post<PlayerSignUp>(this.baseApiUrl+'/api/Players/register',addPlayerRequest);
   }
   signInPlayer(signInRequest:PlayerSignIn):Observable<ActivePlayer>{
+<<<<<<< HEAD
     return this.http.post<ActivePlayer>(this.baseApiUrl+'/api/Players/Login',signInRequest);
   }
   guest():Observable<ActivePlayer>{
     return this.http.post<ActivePlayer>(this.baseApiUrl+'/api/Players/Guest',"");
+=======
+    return this.http.post<ActivePlayer>(this.baseApiUrl+'/api/Players/login',signInRequest);
+    /* DOPISAC FUNKCJE SPRAWDZAJACA CZY KTOS JEST ZALOGOWANY NA TYM KONCIE NA INNYM URZADZENIU I CZY KTOS JEST ZALOGOWANY Z TEGO URZADZENIA */ 
+  }
+  guest():Observable<ActivePlayer>{
+    return this.http.post<ActivePlayer>(this.baseApiUrl+'/api/Players/guest',"");
+    /* DOPISAC FUNKCJE SPRAWDZAJACA CZY KTOS JEST ZALOGOWANY NA TYM KONCIE NA INNYM URZADZENIU I CZY KTOS JEST ZALOGOWANY Z TEGO URZADZENIA */ 
+>>>>>>> parent of 192d8ff (	modified:   src/app/components/games/Dragon/dragon/dragon.component.ts)
   }
   recoveryPlayer(recoveryRequest:EmailRec):Observable<string>{
-    return this.http.put<string>(this.baseApiUrl+'/api/Players/Recovery',recoveryRequest);
+    return this.http.put<string>(this.baseApiUrl+'/api/Players/recovery',recoveryRequest);
   }
   checkJWT(jwt:jwt):Observable<string>{
-    return this.http.put(this.baseApiUrl+'/api/Players/RefreshToken',jwt,{responseType:"text"});
+    return this.http.put(this.baseApiUrl+'/api/Players/refreshToken',jwt,{responseType:"text"});
   }
   logout(jwt:jwt):Observable<any>{
-    return this.http.put(this.baseApiUrl+'/api/Players/Logout',jwt);
+    return this.http.put(this.baseApiUrl+'/api/Players/logout',jwt);
   }
   playerMenu(jwt:jwt): Observable<TableData> {
-    return this.http.post<TableData>(this.baseApiUrl +'/api/Players/GetGamesList',jwt);
+    return this.http.post<TableData>(this.baseApiUrl +'/api/Players/TablesData',jwt);
   }
 }
